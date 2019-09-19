178b69125cfba8d23651ebca26703bd64aab8eeb
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-19 22:36:52 +0100

    Added website data

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export default {
+	menu: [
+		{
+			title: 'About',
+			url: '/about',
+		},
+		{
+			title: 'Portfolio',
+			url: '/about',
+		},
+		{
+			title: 'Skills',
+			url: '/about',
+		},
+	],
+	socials: {
+		twitter: 'https://twitter.com/melquip7',
+		github: 'https://github.com/melquip/',
+		linkedin: 'https://www.linkedin.com/in/melquip/',
+	},
+	portfolio: [
+		{
+			title: '',
+			desc: [
+				'p1',
+				'p2',
+
+			],
+			url_live: '',
+			url_repo: ''
+		}
+	],
+}
